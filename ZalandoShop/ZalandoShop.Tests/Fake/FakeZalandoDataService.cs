7ded71dbed92cf73aaad560b45cca2ba999e51d6
Ahmed Sayed <Ahmed.Sayed@itworx.com>
2017-11-10 07:15:30 +0300

    -Add test case to check fake dataservice.

---
+++ ZalandoShop/ZalandoShop.Tests/Fake/FakeZalandoDataService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZalandoShop.Contracts.Models;
+using ZalandoShop.Contracts.Services;
+using ZalandoShop.Model;
+using ZalandoShop.Shared;
+
+namespace ZalandoShop.Tests.Fake
+{
+    public class FakeZalandoDataService : IZalandoDataService
+    {
+        public IZalandoProductsWithPagingInfo ProductItemsWithPagingInfo;
+        public FakeZalandoDataService()
+        {
+            CreateFakeZalandoData();
+        }
+
+        public void CreateFakeZalandoData()
+        {
+            ProductItemsWithPagingInfo = new ZalandoProductsWithPagingInfo();
+            ProductItemsWithPagingInfo.IZalandoProductItems = new List<ZalandoProductItem>
+            {
+                new ZalandoProductItem
+                {
+                BrandName = "Tommy Hilfiger",
+                ProductName = "Test1",
+                Size = "M",
+                PriceFormatted = "22$"
+                },
+                new ZalandoProductItem
+                {
+                BrandName = "Tommy Hilfiger",
+                ProductName = "Test2",
+                Size = "L",
+                PriceFormatted = "22$"
+                }
+            };
+        }
+        public async Task<IZalandoProductsWithPagingInfo> GetArticlesPaged(string searchKeyWord, FilterType filterType = FilterType.NoFilter, int pageNumber = 1, int pageCount = 20)
+        {
+            return await Task.Factory.StartNew(()=> ProductItemsWithPagingInfo);
+           
+        }
+
+        public Task<ObservableCollection<string>> Search(string searchKeyWord, FilterType filterType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ZalandoShop/ZalandoShop.Tests/UnitTestFakeDataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using ZalandoShop.Shared;
+using ZalandoShop.Contracts.Services;
+using ZalandoShop.Tests.Fake;
+
+namespace ZalandoShop.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+            InstanceFactory.RegisterType<IZalandoDataService, FakeZalandoDataService>();
+        }
+
+
+        [TestMethod]
+        public async void CheckNumberOfItemsInFakeZalandoDataServiceAreTwo()
+        {
+            var fakeDataService = InstanceFactory.GetInstance<IZalandoDataService>();
+            Assert.AreEqual(await fakeDataService.GetArticlesPaged("s", FilterType.NoFilter), 2);
+        }
+    }
+}
